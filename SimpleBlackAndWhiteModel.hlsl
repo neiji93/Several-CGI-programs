30c092f07e3f7b48860a4fc3d12110b71f47174f
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-28 12:02:11 +0100

    Create SimpleBlackAndWhiteModel.hlsl

---
+++ SimpleBlackAndWhiteModel.hlsl
@@ -0,0 +1,8 @@
+
+//according to physics, there must be several explanations about each scene cases.
+//Then, using directly models will be easier because we can extract latent variables.
+//If the complexity of drawing eaach elements of the scene that is concern increase, 
+//we will be able to extract data that are latent variables here. It involves solving many problems
+//To adress control theory without using AI, we will use differential equations. Then, latent variables
+//must be used with differential equations (old researches).
+
