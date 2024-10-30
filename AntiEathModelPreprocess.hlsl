7b7cb50f13254ebecf727a49e2789c6bd1928b25
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-30 09:36:07 +0100

    Update AntiEathModelPreprocess.hlsl

--- AntiEathModelPreprocess.hlsl
+++ AntiEathModelPreprocess.hlsl
@@ -1,4 +1,5 @@
 
+
 //The algorithms behind the market of image manipulation tools are
 //complex and, at the end, nobody has control of the output. This is not
 //really our problem. Our main focus must be generate correct scenes with scene director.
@@ -7,3 +8,10 @@
 //using desaturating images (look CityWorldMapFromCGI.... repository). To merge exp("*x) and desaturate
 //tool, we will use exp(3*x) twice. Then, in physics, we will use exp(3*x) three times....(old maths researches -
 //some kind of absurd reasonning).
+//point de fuite car les morts vivants sont dans le petrin (philosophy).
+
+//navier stokes equations dont belong to this file
+void checkPointDeFuiteSmartPointerTrackEffect(.....)
+{
+
+}
