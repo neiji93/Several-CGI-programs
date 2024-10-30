02278a246e77d72458b50ee24dd9f303530d4d41
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-10-30 09:21:30 +0100

    Create AntiEathModelPreprocess.hlsl

---
+++ AntiEathModelPreprocess.hlsl
@@ -0,0 +1,9 @@
+
+//The algorithms behind the market of image manipulation tools are
+//complex and, at the end, nobody has control of the output. This is not
+//really our problem. Our main focus must be generate correct scenes with scene director.
+//To do that, preventing earth model which is the worst case will be good. exp(3*x) is contained in 
+//some open source tools because it can describe some algorithms and earth model can be avoided by 
+//using desaturating images (look CityWorldMapFromCGI.... repository). To merge exp("*x) and desaturate
+//tool, we will use exp(3*x) twice. Then, in physics, we will use exp(3*x) three times....(old maths researches -
+//some kind of absurd reasonning).
