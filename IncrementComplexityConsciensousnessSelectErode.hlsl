d97da2d7c40da26aa444aa8e6af8b7f53cdd6196
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-11-04 16:26:57 +0100

    Create IncrementComplexityConsciensousnessSelectErode.hlsl

---
+++ IncrementComplexityConsciensousnessSelectErode.hlsl
@@ -0,0 +1,9 @@
+//proof of existence with two person.
+//as we dont need to use plancks constant, we will
+//use log x with exp(3x) once. as this is a' ai,
+//we will have an algorithm  then it means we will
+//incrementroughnes.hlsl. 
+void aiAgentDeletethrreshold(...)
+{
+ //it is best to not ude a threshold because ...
+}
