91efae2713a597355c53e3a24f3a886a2ceb6e92
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-11-16 19:20:32 +0100

    Create MultilayerRecognitionBy.hlsl

---
+++ MultilayerRecognitionBy.hlsl
@@ -0,0 +1,11 @@
+//in philosophy, god may talk and transform
+//humans as metals sometimes with different types
+//this is an alternative to ai
+
+void storyboardCancelRougness();
+
+void storyboardCancelAlbedo()
+
+void storyboardCzncelAllPbr()
+
+void storyboardCancelceramics()
