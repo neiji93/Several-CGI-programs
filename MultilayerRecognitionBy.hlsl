403b0996aba812410d87401aa638ac3e714c6995
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2024-11-17 17:03:36 +0100

    Update MultilayerRecognitionBy.hlsl

--- MultilayerRecognitionBy.hlsl
+++ MultilayerRecognitionBy.hlsl
@@ -2,7 +2,28 @@
 //humans as metals sometimes with different types
 //this is an alternative to ai
 
-void storyboardCancelRougness();
+//metals obey sometimes log(x). clamping roughness is an efficient
+//way to recognize layers that are already described by other metals
+//differential formulas. Plus, storyboard are used for scene directors.
+//that means that this function can be used to create UX design too.
+void storyboardCancelRoughness()
+{
+
+
+  //for UX design. We can take the 6 number of my last particle conductor
+  //to create an expression from scratch that wpuld be an apprxomaten
+//probability said, of futur textures and models generated some formulas
+ //in futur CG or computer Vision papers !
+  //Note here that we forbid using log(x) because it would be better to not increase
+//the number of particle conductors types we can create. As I said, this stuff is for 
+//2024-2025 only. So, so, we stopped using log(x) !
+ if( roughness > exp(0.6) 
+ {
+   //compute some differentials methods !
+ }
+
+
+}
 
 void storyboardCancelAlbedo()
 
